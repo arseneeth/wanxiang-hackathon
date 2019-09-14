383e7a950e0527d504196aa47c459c759037c1cd
0x332 <kinspotting@protonmail.com>
2019-09-14 14:56:42 +0800

    add: poke struct

--- pokesubs/runtime/src/pokesubs.rs
+++ pokesubs/runtime/src/pokesubs.rs
@@ -1,21 +1,39 @@
-use support::{decl_storage, decl_module, StorageValue};
+use support::{decl_storage, decl_module, StorageMap, dispatch::Result};
 use system::ensure_signed;
+use runtime_primitives::traits::{As, Hash};
+use parity_codec::{Encode, Decode};
 
-pub trait Trait: system::Trait {}
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct Poke<Hash, Balance> {
+    id: Hash,
+    dna: Hash,
+    price: Balance,
+    gen: u64,
+}
+
+pub trait Trait: balances::Trait {}
 
 decl_storage! {
     trait Store for Module<T: Trait> as PokeStorage {
-        Value: u64;
+        OwnedPoke get(poke_of_owner): map T::AccountId => Poke<T::Hash, T::Balance>;
     }
 }
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 
-        fn set_value(origin, value: u64) -> Result<(), &'static str> {
+        fn create_poke(origin) -> Result {
+            let sender = ensure_signed(origin)?;
 
-            let _sender = ensure_signed(origin)?;
-            <Value<T>>::put(value);
+            let new_poke = Poke {
+                id: <T as system::Trait>::Hashing::hash_of(&0),
+                dna: <T as system::Trait>::Hashing::hash_of(&0),
+                price: <T::Balance as As<u64>>::sa(0),
+                gen: 0,
+            };
+
+            <OwnedPoke<T>>::insert(&sender, new_poke);
 
             Ok(())
         }
