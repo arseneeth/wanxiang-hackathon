8e576a13e285d2a31a8956bb986e5199243b3258
0x332 <kinspotting@protonmail.com>
2019-09-14 13:28:22 +0800

    add: fixes

--- pokesubs/runtime/src/pokesubs.rs
+++ pokesubs/runtime/src/pokesubs.rs
@@ -1,15 +1,23 @@
-use support::{decl_storage, decl_module};
+use support::{decl_storage, decl_module, StorageValue};
+use system::ensure_signed;
 
 pub trait Trait: system::Trait {}
 
 decl_storage! {
-    trait Store for Module<T: Trait> as KittyStorage {
-        // Declare storage and getter functions here
+    trait Store for Module<T: Trait> as PokeStorage {
+        Value: u64;
     }
 }
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        // Declare public functions here
+
+        fn set_value(origin, value: u64) -> Result<(), &'static str> {
+
+            let _sender = ensure_signed(origin)?;
+            <Value<T>>::put(value);
+
+            Ok(())
+        }
     }
 }
